56a0d47c40a40a349b76b2a37a67893b1809ec09
alejandroet39 <DRUMSINDEEP@GMAIL.COM>
2022-03-08 22:14:04 -0800

    final changes to this hw

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   console.log("hello");
   var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-  // passwordText.value = password;
+  passwordText.value = password;
 
 
 }
@@ -20,22 +20,67 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var low = ["a", "b", "c", "e", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "+", "~", "<", ">", "?", "/"]
-//numcharacter =
-
+var useLength =[];
+let randomPassword =[];
+var userchoice = [];
 //a function named generatePassword
-function generatePassword() {
-  var password_option = [] // it stands for (empty array)
-  var userInputString = prompt("How many characters would you like?")
-  console.log(userInputString)
-  var useLength = parseInt(userInputString)
-  
-  console.log(useLength < 8 )
-  
-  if (useLength < 8 || useLength > 128) {
-    alert("Use a passphrase between 8 & 128 Characters.")
+function generatePassword (){
+  var useLength = parseInt (prompt ("How many characters would you like?"))
+  console.log (typeof useLength)
+  if (useLength <8 || useLength > 128){
+    alert ("Use a passphrase between 8 & 128 Characters.")
     return null
   }
-  //get the preference 
+  var usenum = confirm ("Would you like to include numbers?");
+  var useUpper = confirm ("Would you like to include uppercase?");
+  var useLower = confirm ("Would you like to include lowercase?");
+  var usespChar = confirm ("Would you like to include special characters?");
+  console.log (userchoice);
+  if (usenum ==true){
+    userchoice=userchoice.concat (numbers);
+  }
+  console.log (userchoice);
+  if (useUpper ==true){
+    userchoice=userchoice.concat(caps);
+  }
+  console.log (userchoice);
+  if (useLower ==true){
+    userchoice=userchoice.concat(low);
+  }
+  console.log (userchoice);
+  if (usespChar ==true){
+    userchoice=userchoice.concat(specialCharacters);
+  }
+// this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+// var minimumCount = 0;
+// empty string variable for the for loop below
+var randomPasswordGenerated = [];
+ // loop getting random characters
+ for (let i = 0; i < useLength; i++) {
+  var randomNumberPicked = Math.floor(Math.random() * userchoice.length);
+  randomPasswordGenerated= randomPasswordGenerated + userchoice[randomNumberPicked]
+console.log (randomPasswordGenerated);
+console.log(useLength);
+console.log (randomNumberPicked);
+console.log (i);
+}
+return randomPasswordGenerated;
+}
+//numcharacter =
+// //a function named generatePassword
+// function generatePassword() {
+//   var password_option = [] // it stands for (empty array)
+//   var userInputString = prompt("How many characters would you like?")
+//   console.log(userInputString)
+//   var useLength = parseInt(userInputString)
+  
+//   console.log(useLength < 8 )
+  
+//   if (useLength < 8 || useLength > 128) {
+//     alert("Use a passphrase between 8 & 128 Characters.")
+//     return null
+//   }
+//   //get the preference 
   //identify & collect preference the user has chosen (1 or 4 chosen scopes)
   //create a collection with the chosen scopes,locally scored temp array
   //create a password variable, an array
@@ -43,37 +88,7 @@ function generatePassword() {
   //put the character on the generatePasszVCword variable
   //repeat using forloops, as many times as specified.
   //loop through garanteed elements, replace elements in pasword array. (Math.Random)
-  //join chraccters in the password array into a string.
+  //join characters in the password array into a string.
   //return the password string
-  var upper = confirm("would you like to include uppercase?")
-  // var lower = confirm("Would you like to include lowercase?")
-  // var specialChars = confirm("Would you like to include special characters?")
-  // var num = confirm("would you like to include numbers?")
-  if (upper) {
-    password_option = password_option.concat(caps)
-  }
-
-  // if (num) {
-  //   password_option = password_option.concat(numbers)
-  // }
-
-  // if (upper) {
-  //   password_option = password_option.concat(low)
-  // }
-
-  // if (upper) {
-  //   password_option = password_option.concat(specialCharacters)
-  // }
-
-  console.log(password_option);
-
-
-}
-
-function random_index(array) {
-  var random = Math.floor(Math.random() * array.length)
-  var randomElements = array[random]
-  return randomElements
-}
 
 
