df9ce73ce67d3e691fe90714b5c12a2a4ff38567
alejandroet39 <DRUMSINDEEP@GMAIL.COM>
2022-03-08 16:07:25 -0800

    adding new changes to .js

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("hello");
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
+
 
 }
 
@@ -14,18 +16,23 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //declared in the global scope and is available to all functions 
-  var numbers = [0,1,2,3,4,5,6,7,8,9];
-  var caps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var low = ["a","b","c","e","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var specialCharacters =["!","@","#","$","%","^","&","*","_","+","~","<",">","?","/"]
-  //numcharacter =
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var low = ["a", "b", "c", "e", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "+", "~", "<", ">", "?", "/"]
+//numcharacter =
 
 //a function named generatePassword
-function generatePassword (){
+function generatePassword() {
   var password_option = [] // it stands for (empty array)
-  var uselength = parseInt (prompt ("How many characters would you like?"))
-  if (uselength <8 || uselength > 128){
-    alert ("Use a passphrase between 8 & 128 Characters.")
+  var userInputString = prompt("How many characters would you like?")
+  console.log(userInputString)
+  var useLength = parseInt(userInputString)
+  
+  console.log(useLength < 8 )
+  
+  if (useLength < 8 || useLength > 128) {
+    alert("Use a passphrase between 8 & 128 Characters.")
     return null
   }
   //get the preference 
@@ -38,32 +45,35 @@ function generatePassword (){
   //loop through garanteed elements, replace elements in pasword array. (Math.Random)
   //join chraccters in the password array into a string.
   //return the password string
-    var upper = confirm ("would you like to include uppercase?")
-    var lower = confirm ("Would you like to include lowercase?")
-    var specialChars = confirm ("Would you like to include special characters?")
-    var num = confirm ("would you like to include numbers?")
-    if (upper){
-      password_option = password_option.concat (caps)
-    }
-      
-      if (num){
-        password_option = password_option.concat (numbers)
-      }
+  var upper = confirm("would you like to include uppercase?")
+  // var lower = confirm("Would you like to include lowercase?")
+  // var specialChars = confirm("Would you like to include special characters?")
+  // var num = confirm("would you like to include numbers?")
+  if (upper) {
+    password_option = password_option.concat(caps)
+  }
 
-        if (upper){
-          password_option = password_option.concat (low)
-        }
-          
-        if (upper){
-            password_option = password_option.concat (specialCharacters)
-          }
+  // if (num) {
+  //   password_option = password_option.concat(numbers)
+  // }
 
-    console.log (password_option);
+  // if (upper) {
+  //   password_option = password_option.concat(low)
+  // }
 
-    
+  // if (upper) {
+  //   password_option = password_option.concat(specialCharacters)
+  // }
+
+  console.log(password_option);
+
+
 }
 
-function random_index (array)
+function random_index(array) {
+  var random = Math.floor(Math.random() * array.length)
+  var randomElements = array[random]
+  return randomElements
+}
 
 
-//})
